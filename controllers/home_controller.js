08ea3fdef65c7f4d82d80dc0908e40b8823b2f04
chaitu1969 <chaitanya.karle@gmail.com>
2023-09-07 19:26:05 +0530

    View files added

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,9 @@
 module.exports.home = function(req,res){
-    return res.end('<h1>Home controller for SocialApp</h1>')
+    // return res.end('<h1>Home controller for SocialApp</h1>')
+
+    return res.render('home',{
+        title: "home"
+    })
 };
 
 module.exports.profiles = function(req,res){
